1099ec18770b16990a69eba4fe8041355d5efb6b
TheLisdan <thelisdan@gmail.com>
2025-01-05 21:31:31 +0200

    Add react-router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,18 @@
+import { getLandingRoute, getTodoListRoute } from './lib/routes';
 import { TrpcProvider } from './lib/trpc';
+import { LandingPage } from './pages/LandingPage';
 import { TodoListPage } from './pages/TodoListPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <TodoListPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getLandingRoute()} element={<LandingPage />} />
+          <Route path={getTodoListRoute()} element={<TodoListPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   );
 };
