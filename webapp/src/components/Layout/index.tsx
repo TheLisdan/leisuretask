fde3be71f95e1e4c9c39978d83e2d138704be906
TheLisdan <thelisdan@gmail.com>
2025-01-23 21:14:53 +0200

    Add modal

--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,16 +1,47 @@
 import { Link, Outlet } from 'react-router-dom';
 import { getHomeRoute } from '../../lib/routes';
 import css from './index.module.scss';
+import Modal from 'react-modal';
 import { TodoLogo } from './todo-logo';
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
 
 export const Layout = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
   return (
     <div className={css.layout}>
       <nav className={css.navigation}>
         <Link to={getHomeRoute()} className={css.link}>
           <TodoLogo className={css.logo} />
         </Link>
+        <button onClick={openModal} className={css.openButton}>
+          Открыть модальное окно
+        </button>
       </nav>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className={css.modal}
+        overlayClassName={css.overlay}
+        shouldCloseOnEsc={true}
+        shouldCloseOnOverlayClick={true}
+      >
+        <h2>Модальное окно</h2>
+        <p>Это содержимое модального окна.</p>
+        <button type="button" onClick={closeModal} className={css.closeButton}>
+          Закрыть
+        </button>
+      </Modal>
       <Outlet />
     </div>
   );
--- webapp/src/pages/TodoListPage/index.tsx
+++ webapp/src/pages/TodoListPage/index.tsx
@@ -1,16 +1,17 @@
 import { Sidebar } from '../../components/Sidebar';
-// import { trpc } from '../../lib/trpc';
+import { Task } from '../../components/Task';
+import { trpc } from '../../lib/trpc';
 import css from './index.module.scss';
 
 export const TodoListPage = () => {
-  // const { data, error, isLoading, isFetching, isError } =
-  //   trpc.getTasks.useQuery();
-  // if (isLoading && isFetching) {
-  //   return <span>Loading...</span>;
-  // }
-  // if (isError) {
-  //   return <span>Error: {error?.message}</span>;
-  // }
+  const { data, error, isLoading, isFetching, isError } =
+    trpc.getTasks.useQuery();
+  if (isLoading && isFetching) {
+    return <span>Loading...</span>;
+  }
+  if (isError) {
+    return <span>Error: {error?.message}</span>;
+  }
 
   return (
     <>
@@ -23,6 +24,7 @@ export const TodoListPage = () => {
         <h1 className={css.bigText}>
           <b>2 hours</b> of free time remaining
         </h1>
+        {data?.tasks.map((task) => <Task task={task} key={task.id} />)}
       </div>
     </>
   );
