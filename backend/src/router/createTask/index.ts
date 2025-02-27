82df9c479da4dbea7fb97716888f05822d2eb991
TheLisdan <thelisdan@gmail.com>
2025-02-27 21:15:25 +0200

    Connected task to users and made tasks unique for every user

--- backend/src/router/createTask/index.ts
+++ backend/src/router/createTask/index.ts
@@ -4,8 +4,12 @@ import { zCreateTaskTrpcInput } from './input';
 export const createTaskTrpcRoute = trpc.procedure
   .input(zCreateTaskTrpcInput)
   .mutation(async ({ input, ctx }) => {
+    if (!ctx.me) {
+      throw new Error('UNAUTHORIZED');
+    }
     await ctx.prisma.task.create({
       data: {
+        userId: ctx.me.id,
         title: input.title,
         status: 'IN_PROGRESS',
       },
--- backend/src/router/getTasks/index.ts
+++ backend/src/router/getTasks/index.ts
@@ -1,7 +1,13 @@
 import { trpc } from '../../lib/trpc';
 
 export const getTasksTrpcRoute = trpc.procedure.query(async ({ ctx }) => {
+  if (!ctx.me) {
+    throw new Error('Not authenticated');
+  }
   const tasks = await ctx.prisma.task.findMany({
+    where: {
+      userId: ctx.me?.id,
+    },
     select: {
       id: true,
       title: true,
