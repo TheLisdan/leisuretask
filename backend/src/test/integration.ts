7576d2b3f88876738e689c6d95686867054f7aed
TheLisdan <thelisdan@gmail.com>
2025-05-21 12:04:39 +0300

    Some refactoring and helpers for backend integration tests

---
+++ backend/src/test/integration.ts
@@ -0,0 +1,94 @@
+import { type Task, type User } from '@prisma/client';
+import _ from 'lodash';
+import { createAppContext } from '../lib/ctx';
+import { getTrpcContext } from '../lib/trpc';
+import { trpcRouter } from '../router';
+import { deepMap } from '../utils/deepMap';
+import { getPasswordHash } from '../utils/getPasswordHash';
+import { type ExpressRequest } from '../utils/types';
+
+export const appContext = createAppContext();
+
+afterAll(appContext.stop);
+
+beforeEach(async () => {
+  await appContext.prisma.task.deleteMany();
+  await appContext.prisma.user.deleteMany();
+});
+
+export const getTrpcCaller = (user?: User) => {
+  const req = { user } as ExpressRequest;
+  return trpcRouter.createCaller(getTrpcContext({ appContext, req }));
+};
+
+export const withoutNoize = (input: any): any => {
+  return deepMap(input, ({ value }) => {
+    if (_.isObject(value) && !_.isArray(value)) {
+      return _.entries(value).reduce(
+        (acc, [objectKey, objectValue]: [string, any]) => {
+          if ([/^id$/, /Id$/, /At$/].some((regex) => regex.test(objectKey))) {
+            return acc;
+          }
+          return {
+            ...acc,
+            [objectKey]: objectValue,
+          };
+        },
+        {}
+      );
+    }
+    return value;
+  });
+};
+
+export const createUser = async ({
+  user = {},
+  number = 1,
+}: { user?: Partial<User>; number?: number } = {}) => {
+  return await appContext.prisma.user.create({
+    data: {
+      name: `user${number}`,
+      email: `user${number}@example.com`,
+      passwordHash: user.passwordHash || getPasswordHash('1234'),
+      ..._.omit(user, ['password']),
+    },
+  });
+};
+
+export const createTask = async ({
+  task = {},
+  author,
+  number = 1,
+}: {
+  task?: Partial<Task>;
+  author: Pick<User, 'id'>;
+  number?: number;
+}) => {
+  return await appContext.prisma.task.create({
+    data: {
+      userId: author.id,
+      title: `task${number}`,
+      status: 'IN_PROGRESS',
+      award: 60 * number,
+      order: number,
+      ...task,
+    },
+  });
+};
+
+export const createTaskWithAuthor = async ({
+  author,
+  task,
+  number,
+}: {
+  author?: Partial<User>;
+  task?: Partial<Task>;
+  number?: number;
+} = {}) => {
+  const createdUser = await createUser({ user: author, number });
+  const createdTask = await createTask({ task, author: createdUser, number });
+  return {
+    author: createdUser,
+    task: createdTask,
+  };
+};
