723b1e252e651f4a195149ee1084dbd6d15e9496
TheLisdan <thelisdan@gmail.com>
2025-06-03 23:47:39 +0300

    Add autoprefixer

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,6 +1,7 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig, loadEnv } from 'vite';
 import { parsePublicEnv } from './src/lib/parsePublicEnv';
@@ -41,6 +42,11 @@ export default defineConfig(({ mode }) => {
             release: { name: env.SOURCE_VERSION },
           }),
     ],
+    css: {
+      postcss: {
+        plugins: [autoprefixer({})],
+      },
+    },
     build: {
       rollupOptions: {
         output: {
